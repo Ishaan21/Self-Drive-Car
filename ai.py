fc4a80f4249ff2f59b195ed7e559ddf21ef8b85c
ishaan <ishaanjain21@gmail.com>
2021-02-03 12:20:36 +0530

    Architecture of AI

--- ai.py
+++ ai.py
@@ -10,3 +10,11 @@ import torch.optim as optim
 import torch.autograd as autograd
 from torch.autograd import Variable
 
+class Network(nn.Module):
+    
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_action)
